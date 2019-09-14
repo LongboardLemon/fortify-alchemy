0c7fb3dca54ec408312ce325ee5a83290727fcf8
MilesG <longboardlemon@gmail.com>
2019-09-14 13:34:37 -0700

    add matching ingredients to effects page

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import IngredientTile from "./IngredientTile";
 import Layout from "./Layout";
 
 const Effect = ({ data }) => {
@@ -10,6 +11,11 @@ const Effect = ({ data }) => {
                 <h1>{data.effectsJson.name}</h1>
                 <p>{data.effectsJson.description}</p>
             </div>
+            <div>
+                {data.allIngredientsJson && data.allIngredientsJson.edges.map((ingredient, i) => (
+                    <IngredientTile key={i} {...ingredient.node} />
+                ))}
+            </div>
         </Layout>
     );
 };
@@ -18,6 +24,19 @@ export default Effect;
 
 export const pageQuery = graphql`
     query EffectQuery($slug: String) {
+        allIngredientsJson(filter: { effects: { elemMatch: { slug: { eq: $slug } } } }) {
+            edges {
+                node {
+                    effects {
+                        name
+                        poison
+                        slug
+                    }
+                    name
+                    slug
+                }
+            }
+        }
         effectsJson(slug: { eq: $slug }) {
             description
             effect
