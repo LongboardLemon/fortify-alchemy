fa31c8f446862f6c2f25f3d9e74a02936cfa2164
MilesG <longboardlemon@gmail.com>
2019-09-26 18:00:52 -0700

    create search context and temporary sidebar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,21 @@
 import React from "react";
 
 import Link from "./Link";
-import SearchContainer from "./SearchInput";
+import SearchInput from "./SearchInput";
 
-const Navigation = props => {
-    return (
-        <div style={styles.root}>
-            <SearchContainer />
-            <Link
-                to="/ingredients">
-                Ingredients
-            </Link>
-            <Link
-                to="/effects">
-                Effects
-            </Link>
-        </div>
-    );
-};
+const Navigation = props => (
+    <div style={styles.root}>
+        <SearchInput />
+        <Link
+            to="/ingredients">
+            Ingredients
+        </Link>
+        <Link
+            to="/effects">
+            Effects
+        </Link>
+    </div>
+);
 
 const styles = {
     root: {
