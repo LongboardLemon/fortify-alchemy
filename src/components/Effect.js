923ac7635e8775be0458e18b05932f9a6f14d52f
MilesG <longboardlemon@gmail.com>
2019-11-06 18:12:18 -0800

    style and structure for effect and ingredient detail pages

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -1,50 +1,69 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
-import IngredientTile from "./IngredientTile";
+import IngredientGrid from "./IngredientGrid";
 import Layout from "./Layout";
+import SchoolAvatar from "./SchoolAvatar";
 
-const Effect = ({ data }) => {
+const EffectDetail = ({ effect }) => {
+    let styles = effectStyles();
     return (
-        <Layout>
-            <div >
-                <h1>{data.effectsJson.name}</h1>
-                <p>{data.effectsJson.description}</p>
-            </div>
-            <div>
-                {data.allIngredientsJson && data.allIngredientsJson.edges.map((ingredient, i) => (
-                    <IngredientTile key={i} {...ingredient.node} />
-                ))}
-            </div>
-        </Layout>
+        <Card className={styles.effectCard} raised>
+            <CardHeader
+                avatar={
+                    <SchoolAvatar
+                        className={styles.avatar}
+                        iconProps={{ className: styles.icon }}
+                        school={effect.school} />
+                }
+                subheader={effect.effect}
+                title={
+                    <Typography variant="h4">{effect.name}</Typography>
+                } />
+            <CardContent>
+                {effect.description}
+            </CardContent>
+        </Card>
     );
 };
+
+const Effect = ({ data }) => (
+    <Layout>
+        <EffectDetail effect={data.effectsJson} />
+        <IngredientGrid ingredients={data.effectsJson.ingredients} />
+    </Layout>
+);
+
+const effectStyles = makeStyles(theme => ({
+    avatar: {
+        height: 80,
+        width: 80,
+    },
+    effectCard: {
+        marginBottom: theme.spacing(2),
+        width: "100%",
+    },
+    icon: {
+        height: "2em",
+        width: "2em",
+    },
+}));
 
 export default Effect;
 
 export const pageQuery = graphql`
     query EffectQuery($slug: String) {
-        allIngredientsJson(filter: { effects: { elemMatch: { slug: { eq: $slug } } } }) {
-            edges {
-                node {
-                    effects {
-                        name
-                        poison
-                        slug
-                    }
-                    itemID
-                    name
-                    slug
-                    value
-                    weight
-                }
-            }
-        }
         effectsJson(slug: { eq: $slug }) {
             description
             effect
+            ingredients {
+                ...ingredientTile
+            }
             name
             poison
+            school
         }
     }
 `;
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,20 +1,104 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { Box, Card, CardContent, CardHeader, Chip, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
+import IngredientAvatar from "./IngredientAvatar";
+import IngredientGrid from "./IngredientGrid";
+import { IngredientAttributes } from "./IngredientTile";
 import Layout from "./Layout";
+import SchoolAvatar from "./SchoolAvatar";
 
-const Ingredient = ({ data }) => {
+const IngredientDetail = ({ ingredient }) => {
+    let styles = ingredientStyles();
     return (
-        <Layout>
-            <div style={styles.root}>
-                <h1>{data.ingredientsJson.name}</h1>
-                <p>{data.ingredientsJson.description}</p>
-            </div>
-        </Layout>
+        <Card classes={{ root: `${styles.flexColumn} ${styles.ingredientCard}` }} raised>
+            <CardHeader
+                avatar={
+                    <IngredientAvatar
+                        className={styles.avatar}
+                        iconProps={{ className: styles.icon }}
+                        type={ingredient.type} />
+                }
+                classes={{
+                    content: styles.headerContent,
+                    subheader: styles.attributes,
+                }}
+                subheader={
+                    <IngredientAttributes
+                        value={ingredient.value}
+                        weight={ingredient.weight} />
+                }
+                title={
+                    <Typography variant="h4">{ingredient.name}</Typography>
+                } />
+            <CardContent>
+                {ingredient.description}
+            </CardContent>
+        </Card>
     );
 };
 
-let styles = {};
+const EffectsSummary = ({ ingredient }) => {
+    let styles = ingredientStyles();
+    return (
+        <Box className={styles.flexColumn}>
+            {ingredient.effects.map((effect, i) => {
+                let ingredientArray = effect.ingredients.filter(item => item.slug !== ingredient.slug);
+                return (
+                    <Box key={i}>
+                        <Chip
+                            className={styles.effectDivider}
+                            icon={<SchoolAvatar school={effect.school} />}
+                            label={effect.name} />
+                        <IngredientGrid ingredients={ingredientArray} />
+                    </Box>
+                );
+            })}
+        </Box>
+    );
+};
+
+const Ingredient = ({ data }) => (
+    <Layout>
+        <IngredientDetail ingredient={data.ingredientsJson} />
+        <EffectsSummary ingredient={data.ingredientsJson} />
+    </Layout>
+);
+
+const ingredientStyles = makeStyles(theme => ({
+    attributes: {
+        display: "flex",
+        width: 228,
+        [theme.breakpoints.down("xs")]: {
+            width: "auto",
+        },
+    },
+    avatar: {
+        height: 80,
+        width: 80,
+    },
+    effectDivider: {
+        margin: theme.spacing(2, 0),
+        width: "100%",
+    },
+    flexColumn: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+    },
+    icon: {
+        height: "2em",
+        width: "2em",
+    },
+    ingredientCard: {
+        marginBottom: theme.spacing(2),
+        width: "100%",
+    },
+    headerContent: {
+        flex: "0 1 auto",
+    },
+}));
 
 export default Ingredient;
 
@@ -23,11 +107,16 @@ export const pageQuery = graphql`
         ingredientsJson(slug: { eq: $slug }) {
             description
             effects {
+                ingredients {
+                    ...ingredientTile
+                }
                 name
                 poison
+                school
                 slug
             }
             name
+            type
             value
             weight
         }
