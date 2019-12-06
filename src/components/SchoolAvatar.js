3f9187ec12be13ccff99c7592312f5a8581dde24
MilesG <longboardlemon@gmail.com>
2019-12-06 14:56:51 -0800

    color school icons and create simple home page

--- src/components/SchoolAvatar.js
+++ src/components/SchoolAvatar.js
@@ -7,18 +7,39 @@ import {
 } from "./icons";
 
 const SchoolAvatar = ({ iconProps, school, ...props }) => {
-    let schoolIcons = {
-        Alteration: AlterationIcon,
-        Conjuration: ConjurationIcon,
-        Destruction: DestructionIcon,
-        Illusion: IllusionIcon,
-        Mysticism: MysticismIcon,
-        Restoration: RestorationIcon,
+    let avatarProps = {
+        Alteration: {
+            icon: AlterationIcon,
+            gradient: "#9a48b4, #702587",
+        },
+        Conjuration: {
+            icon: ConjurationIcon,
+            gradient: "#897f37, #564e07",
+        },
+        Destruction: {
+            icon: DestructionIcon,
+            gradient: "#ca463a, #810805",
+        },
+        Illusion: {
+            icon: IllusionIcon,
+            gradient: "#537653, #345635",
+        },
+        Mysticism: {
+            icon: MysticismIcon,
+            gradient: "#795f7d, #513958",
+        },
+        Restoration: {
+            icon: RestorationIcon,
+            gradient: "#616ca2, #2f4072",
+        },
     };
-    let SchoolIcon = schoolIcons[school];
+    let SchoolIcon = avatarProps[school].icon;
     if (!SchoolIcon) return null;
     return (
-        <Avatar alt={school} {...props}>
+        <Avatar
+            alt={school}
+            style={{ backgroundImage: `radial-gradient(${avatarProps[school].gradient})` }}
+            {...props}>
             <SchoolIcon color="primary" {...iconProps} />
         </Avatar>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,57 @@
 import React from "react";
+import { Avatar, Box, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 import Layout from "../components/Layout";
+import Link from "../components/Link";
+import { PotionIcon, SaltIcon } from "../components/icons";
 
-const IndexPage = () => (
-    <Layout>
-        <h1>Yo what it do</h1>
-    </Layout>
-);
+const IndexPage = props => {
+    let styles = indexStyles();
+    return (
+        <Layout>
+            <Box className={styles.root}>
+                <Typography variant="h1">
+                    Fortify Alchemy
+                </Typography>
+                <Typography variant="h4">
+                    A simple alchemy tool for TES III: Morrowind.
+                </Typography>
+                <Box className={styles.homeLinks}>
+                    <Link className={styles.homeLink} to="/effects">
+                        <Avatar alt="Effects">
+                            <PotionIcon color="primary" />
+                        </Avatar>
+                        Effects
+                    </Link>
+                    <Link className={styles.homeLink} to="/ingredients">
+                        <Avatar alt="Ingredients">
+                            <SaltIcon color="primary" />
+                        </Avatar>
+                        Ingredients
+                    </Link>
+                </Box>
+            </Box>
+        </Layout>
+    );
+};
+const indexStyles = makeStyles(theme => ({
+    root: {
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        marginTop: 80,
+        width: "100%",
+    },
+    homeLink: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: 24,
+    },
+    homeLinks: {
+        display: "flex",
+    },
+}));
 
 export default IndexPage;
